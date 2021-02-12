c1d90a836b89bca44ad32353449aa05ce140c380
volodyatan <volodyatan@yahoo.ca>
2021-02-12 10:37:13 -0500

    changed context to for presentation

--- context.js
+++ context.js
@@ -13,7 +13,7 @@ const context = {
                 2: `All templates and states`
             },
             button: {
-                1: {text: "ok", actionID: 111},
+                1: {text: "submit", actionID: 111},
                 2: {text: "vote", actionID: 222}
             },
             image: {
@@ -26,25 +26,23 @@ const context = {
         "fr-CA": {
             title: {
                 1: {text: "Menu principal"},
-                2: {text: "Changer d'état"},
-                3: {text: "Obtenez des données"}
+                2: {text: "Changer d'état"}
+            },
+            header: {
+                1: {text: "Entête"}
             },
             text: {
-                //defaults
-                1: `Tous les États`,
-                2: `Tous les modèles et états`,
-                3: `Transition réussie`,
-                4: `Diffusion réussie`,
-                5: `La diffusion a échoué`,
-                6: `Données ajoutées au modèle avec succès`,
-                7: `L'élément n'existe PAS dans le modèle`,
-                8: `Données demandées récupérées`,
-                //user defined
-
+                1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
             },
             button: {
                 1: {text: "ok", actionID: 111},
                 2: {text: "vote", actionID: 222}
+            },
+            image: {
+                1: {
+                    "alt-text": "photo de chat mignon",
+                    "src": "https://www.sustainableplaces.eu/wp-content/uploads/2017/02/SmartBuilding.jpg"
+                }
             }
         }
     },
